7cc4cbeb8ca5f664e58fe6117248613a7ee758ad
TristanMngr <tristanmenager@gmail.com>
2020-10-19 20:49:25 +0200

    replace sqlit by postgresql, add chartkick, add haml

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,14 +8,24 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "bootstrap";
-import "../stylesheets/application"  // import scss file
+// design system
+require("bootstrap")
+
+// chartkick for graph
+require("chartkick")
+require("chart.js")
+
+// stylesheets
+import "../stylesheets/application"  
+
+// custom js files
+require("packs/homepages")
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
+  
 })
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
